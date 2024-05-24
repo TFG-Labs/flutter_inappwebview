38108d9528df5b2dfe3d7f830a232c0c17dba469
Larry Stent <lawrencestent@gmail.com>
2024-05-24 11:52:41 +0200

    work on update

--- flutter_inappwebview_ios/example/ios/Flutter/flutter_export_environment.sh
+++ flutter_inappwebview_ios/example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/lawrences/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/lawrences/work/flutter_inappwebview/example"
+export "FLUTTER_APPLICATION_PATH=/Users/lawrences/work/flutter_inappwebview/flutter_inappwebview_ios/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
